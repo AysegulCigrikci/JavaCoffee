cef1c30bd144097847ebc19379b7360e0d801bec
AysegulCigrikci <35061988+AysegulCigrikci@users.noreply.github.com>
2021-06-05 18:10:09 +0300

    Add files via upload

---
+++ coffe/src/Adapters/MernisServiceAdapter.java
@@ -0,0 +1,35 @@
+package Adapters;
+
+import java.rmi.RemoteException;
+import java.time.ZoneId;
+import Abstract.CustomerCheckService;
+import Entities.Customer;
+import tr.gov.nvi.tckimlik.WS.KPSPublicSoapProxy;
+
+public class MernisServiceAdapter implements CustomerCheckService{
+
+	@Override
+	public boolean CheckIfRealPerson(Customer customer) {
+
+		KPSPublicSoapProxy kpsPublicSoapProxy = new KPSPublicSoapProxy();    
+	    boolean result=false;
+	    
+		try 
+		{
+			result = kpsPublicSoapProxy.TCKimlikNoDogrula
+					(
+					Long.parseLong(customer.getNationalityId()) , 
+					customer.getFirstName().toUpperCase(), 
+					customer.getLastName().toUpperCase(), 
+					customer.getDateOfBirth().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().getYear()
+					);
+		} 
+		catch (RemoteException e) 
+		{
+			System.out.println("mernis çalýþmýyor");
+		}
+
+		return result;
+	}
+
+}
